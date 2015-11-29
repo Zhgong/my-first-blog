2a209b511d95dfb9525f2cde68badddf3b8b6e38
seangongz <sean.gongz@gmail.com>
2015-11-29 17:59:28 +0100

    Homework: add more to your website

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,7 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
+            # post.create_date = timezone.now() # this one is better, firt edit and save, then publish
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
@@ -42,3 +43,17 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
